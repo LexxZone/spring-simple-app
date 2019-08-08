26444ac1da86ce054864bee928a84f7f0a9374d0
Alexey Dvoryaninov <alexey.dvoryaninov@devexperts.com>
2019-08-08 21:19:17 +0300

    Spring Boot project structure was added.

---
+++ HELP.md
@@ -0,0 +1,6 @@
+### Yet Another Spring-Boot App
+
+#### Here you can meet any helpful info for this project.
+
+
+                                           Alexey D.
---
+++ src/main/java/ru/stateofmind/simplespringapp/SimpleSpringAppApplication.java
@@ -0,0 +1,13 @@
+package ru.stateofmind.simplespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringAppApplication.class, args);
+	}
+
+}
